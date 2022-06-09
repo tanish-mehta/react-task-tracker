1ad7447e2ab8f41ec34b4383cf789b704f28c916
loft36 <72663094+loft36@users.noreply.github.com>
2022-06-09 01:58:36 +0100

    before db

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
+import { useState } from "react"
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'food shopping',
+      day: 'today',
+      reminder: false,
+    },
+    {
+      id: 2,
+      text: 'food shopping',
+      day: 'today',
+      reminder: false,
+    },
+    {
+      id: 3,
+      text: 'food shopping',
+      day: 'today',
+      reminder: false,
+    }
+  ])
+
+  //delete task 
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id ? {} : task))
+  }
+
+  // toggle reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
+  }
+
+  //add task
+  const addTask = (task) => {
+    const id = 10
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <Header onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask} />
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ?
+        <Tasks tasks={tasks} onD elete={deleteTask} onToggle={toggleReminder} />
+        : 'No Tasks'}
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
+const Header = ({ title, onAdd, showAdd }) => {
+
+    return (
+        <header class="header">
+            <h1>{title}</h1>
+            <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd} />
+        </header>
+    )
+}
+
+Header.defaultProps = {
+    title: 'Task Tracker',
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+
+// const headingStyle = {
+//     color: 'red',
+//     backgroundColor: 'black', 
+// }
+export default Header
